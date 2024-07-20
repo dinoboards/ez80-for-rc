686437e8ed5bc70c699e943a4bcba7c7f4f0c062
Dean Netherton <dinoboards@netherton.org>
2024-07-20 10:26:24 +1000

    RST %10: implementing platform init/query function

--- src/rst-10-drivers/ez80_utils.s
+++ src/rst-10-drivers/ez80_utils.s
@@ -7,6 +7,10 @@
 
 	PUBLIC	ez80_utils_control
 	XREF	_system_ticks
+	XREF	_platform_description
+	XREF	_SYS_CLK_FREQ
+	XREF	_cpu_freq_calculated
+	XREF	__ldivu
 
 ez80_utils_control:
 	POP	BC					; RESTORE BC AND HL
@@ -30,19 +34,78 @@ ez80_utils_control:
 ;   MBASE:HL{15:0} = Pointer to the ez80 firmware configuration table
 ;
 ; Input Table:
-;  00-03: HBIOS Version?
-;  04-07: CPU Build Frequency
-;  08-15: RESERVED
+;  00-03: CB_VERSION: 		HBIOS Version (RMJ, RMN, RUP, RTP)
+;  04-07: CPUOSC: 		CPU Build Frequency
+;  08-08: CB_PLATFORM: 		Platform ID
+;  09-09: MEMMGR: 		Memory manager id
+;  10-11: RAMSIZE:		Total size in KB
+;  12-13: ROMSIZE:		Total size in KB
+;  14-15: RESERVED
 ;
 ; Output Table:
-;  00-03: Firmware version (Major, Minor, Patch)??
-;  04-07: CPU Build Frequency
-;  08-11: CPU Evaluated Frequency
-;  12-12: CHIP CODE (0->EZ80F92AZ020EG)
-;  13-15: RESERVED
+;  00-03: EZ80_PLT_EZ80VER: 	Firmware version (Major, Minor, Patch)??
+;  04-07: EZ80_PLT_CPUOSC: 	CPU Evaluated Frequency
+;  08-08: EZ80_PLT_CPUMHZ: 	(CPU Evaluated Frequency / 1000000)
+;  09-10: EZ80_PLT_CPUKHZ: 	(CPU Evaluated Frequency / 1000)
+;  11-11: EZ80_PLT_CHIP_ID: 	(0->EZ80F92AZ020EG)
+;  12-15: RESERVED
+;
 ez80_firmware_query:
-	LD	HL, %000001 ; 0.0.0
+	LD	B, 16
+	LD	DE, _platform_description
+	PUSH	HL
+copy_platform_tbl:
+	LD.S	A, (HL)
+	LD	(DE), A
+	INC	HL
+	INC	DE
+	DJNZ	copy_platform_tbl
+
+	POP	IX
+	PUSH	IX
+
+	LD.S	(IX+0), 0				; ASSIGN VERSION NUMBER 0.0.0.1
+	LD.S	(IX+1), 0
+	LD.S	(IX+2), 0
+	LD.S	(IX+3), 1
+
+	LD	A, (_cpu_freq_calculated)
+	LD.S	(IX+4), A
+	LD	A, (_cpu_freq_calculated+1)
+	LD.S	(IX+5), A
+	LD	A, (_cpu_freq_calculated+2)
+	LD.S	(IX+6), A
+	LD	A, (_cpu_freq_calculated+3)
+	LD.S	(IX+7), A
+
+	LD	HL, (_cpu_freq_calculated)
+	LD	E, A
+	LD	BC, 1000000
+	XOR	A
+	CALL	__ldivu
+	LD.S	(IX+8), L
+
+	LD	HL, (_cpu_freq_calculated)
+	LD	A, (_cpu_freq_calculated+3)
+	LD	E,A
+	LD	BC, 1000
+	XOR	A
+	CALL	__ldivu
+	LD.S	(IX+9), L
+	LD.S	(IX+10), H
+
+	XOR	A					; ZERO OUT THE REMAINING BYTES
+	LD.S	(IX+11), A
+	LD.S	(IX+12), A
+	LD.S	(IX+13), A
+	LD.S	(IX+14), A
+	LD.S	(IX+15), A
+
+	POP	HL					; RESTORE TABLE ADDR
 	RET.L
+
+_system_build_freq:
+	DL	_SYS_CLK_FREQ
 ;
 ; Function B = 01 -- register copy (8:16 -> 24)
 ; LD HL, E:HL{15:0}
--- src/startup/system_vars.s
+++ src/startup/system_vars.s
@@ -6,10 +6,44 @@
 
 	PUBLIC	_rtc_enabled
 	PUBLIC	_cpu_freq_calculated
+	PUBLIC	_platform_description
+	PUBLIC	_romwbw_version
+	PUBLIC	_romwbw_version_major
+	PUBLIC	_romwbw_version_minor
+	PUBLIC	_romwbw_version_patch
+	PUBLIC	_romwbw_version_revision
+	PUBLIC	_romwbw_cpu_frequency
+	PUBLIC	_romwbw_platform_code
+	PUBLIC	_romwbw_memory_manager_code
+	PUBLIC	_romwbw_ramsize
+	PUBLIC	_romwbw_romsize
 
 _rtc_enabled:
 	DB	0
 
 _cpu_freq_calculated:
 	DS	4
+
+_platform_description:
+_romwbw_version:
+_romwbw_version_major:
+	DS	1
+_romwbw_version_minor:
+	DS	1
+_romwbw_version_patch:
+	DS	1
+_romwbw_version_revision:
+	DS	1
+_romwbw_cpu_frequency:
+	DS	4
+_romwbw_platform_code:
+	DS	1
+_romwbw_memory_manager_code:
+	DS	1
+_romwbw_ramsize:
+	DS	2
+_romwbw_romsize:
+	DS	2
+_romwbw_reserved:
+	DS	2
 
