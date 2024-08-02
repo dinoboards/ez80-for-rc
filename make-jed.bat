c9eec5e49917f28cb231d0b6f14aa40ad67fef9a
Dean Netherton <dinoboards@netherton.org>
2024-08-02 15:24:07 +1000

    added pld code for WR bus hold control

---
+++ make-jed.bat
@@ -0,0 +1,30 @@
+@echo off
+
+SETLOCAL
+
+if "%CUPLPATH%" == "" set CUPLPATH=z:\Wincupl\Shared
+
+set PLD_SRC=%1
+
+if "%PLD_SRC%" == "" set PLD_SRC=ez80-cpu
+
+cd hardware
+
+if not exist "bin" mkdir bin
+
+echo Compiling %PLD_SRC%.jed from %PLD_SRC%.pld
+echo.
+
+%CUPLPATH%\cupl.exe -e -x -f - -ju %CUPLPATH%\cupl.dl %PLD_SRC%.pld
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+REM Removed the location entry - the TL866II Plus programmer seems to have loading file issue
+
+findstr /v /i /L /c:"Location" %PLD_SRC%.jed >bin\%PLD_SRC%.jed
+
+move %PLD_SRC%.doc bin\%PLD_SRC%.doc
+move %PLD_SRC%.mx bin\%PLD_SRC%.mx
+
+del %PLD_SRC%.jed
+
+echo.
