2076a2089923b5555d4c39a9f27e310bf3f1ef62
Dean Netherton <dinoboards@netherton.org>
2024-09-15 15:42:05 +1000

    rst-10: deleted and marked as deprecated functions no longer used/required

--- firmware/src/rst-10-drivers/rst-10-00-system-utils-functions.s
+++ firmware/src/rst-10-drivers/rst-10-00-system-utils-functions.s
@@ -46,17 +46,17 @@ _system_utils_dispatch:
 	OR	A					; TEST SUB FUNCTION CODE
 	JR	Z, ez80_version_exchange		; B = 0, SYSUTL_VER_EXCHANGE
 	DEC	A
-	JR	Z, ez80_bus_cycles_get			; B = 1, SYSUTL_BUSTM_GET
+	JR	Z, not_supported			; B = 1, DEPRECATED
 	DEC	A
-	;JR	Z, ...					; B = 2, DEPRECATED
+	JR	Z, not_supported			; B = 2, DEPRECATED
 	DEC	A
-	JR	Z, ez80_bus_cycles_set			; B = 3, SYSUTL_BUSTM_SET
+	JR	Z, not_supported			; B = 3, DEPRECATED
 	DEC	A
-	JR	Z, ez80_bus_freq_set			; B = 4, SYSUTL_BUSFQ_SET
+	JR	Z, not_supported			; B = 4, DEPRECATED
 	DEC	A
 	JR	Z, ez80_cpu_freq_get			; B = 5, SYSUTL_CPU_FREQ_GET
 	DEC	A
-	JR	Z, ez80_bank_helper_set			; B = 6, SYSUTL_BANK_HELPER_SET
+	JR	Z, not_supported			; B = 6, DEPRECATED
 	DEC	A
 	JR	Z, ez80_debug				; B = 7, SYSUTL_DEBUG
 
@@ -82,6 +82,7 @@ _system_utils_dispatch:
 	DEC	A					; B = 16, SYSUTL_FLSHFQ_SET
 	JR	Z, ez80_flsh_freq_set
 
+not_supported:
 	LD	A, %FF					; UNKNOWN FUNCTION
 	RET.L
 ;
@@ -136,22 +137,14 @@ ENDIF
 	XOR	A
 	RET.L
 ;
-; Function B = 01 -- SYSUTL_BUSTM_GET
-; Get Bus cycles for external memory and I/O
+; Function B = 01 -- DEPRECATED
+;
 ;
 ; Input
 ;  None
 ;
 ; Output
-; H	= CS3 External Memory Bus Cycles (1-15)
-; L	= CS2 External I/O Bus Cycles (1-15)
-; DE	= 0
-; A	= 0 -> SUCCESS
-ez80_bus_cycles_get:
-	LD	HL, (_cs_bus_timings)
-	LD	DE, 0
-	XOR	A
-	RET.L
+;
 
 ; Function B = 02 -- DEPRECATED
 ;
@@ -161,144 +154,25 @@ ez80_bus_cycles_get:
 ;
 ; Output
 ;
-	SECTION	CODE
-;
-; Function B = 03 -- SYSUTL_BUSTM_SET
-; Set Bus cycles for external memory and I/O
-;
-; Input
-;   H 	= CS3 External Memory Bus Cycles (1-15)
-;   L 	= CS2 External I/O Bus Cycles (1-15)
-;
-; Output
-;   H 	= CS3 External Memory Bus Cycles (1-15)
-;   L 	= CS2 External I/O Bus Cycles (1-15)
-;   A 	= 0 -> SUCCESS, NZ -> ONE ORE MORE VALUES OUT OF RANGE
-;
-ez80_bus_cycles_set:
-	PUSH	IX
-	LD	IX, _cs_bus_timings
 
-	XOR	A					; IO must always be >= 1 & <= 15
-	OR	L
-	JR	NZ, valid_io_bus_cycle
-	LD	L,1
-valid_io_bus_cycle:
-
-	XOR	A					; MEM must always be >= 1 & <= 15
-	OR	H
-	JR	NZ, valid_mem_bus_cycle
-	LD	H, 1
-valid_mem_bus_cycle:
-	LD	(IX+0), L
-	LD	(IX+1), H
-
-	; Assign CS2 bus mode
-	LD	A, L
-	AND	%0F
-	OR	BMX_BM_Z80 | BMX_AD_SEPERATE
-	OUT0	(CS2_BMC), A
-
-	; Assign CS3 bus mode
-	LD	A, H
-	AND	%0F
-	OR	BMX_BM_Z80 | BMX_AD_SEPERATE
-	OUT0	(CS3_BMC), A
-
-	POP	IX
-	XOR	A
-	RET.L
 ;
-; Function B = 04 --SYSUTL_BUSFQ_SET
-; Set Bus cycles to based on an equivalent frequency calculation.
-; If the calculated values are below the supplied minimum values (H', L')
-; the minimum values are used.
+; Function B = 03 -- DEPRECATED
+;
 ;
 ; Input
-;   HL = CS3 External Memory Bus Frequency (Khz)
-;   DE = CS2 External I/O Bus Frequency (Khz)
-;   H' = Minimum CS3 External Memory Bus Cycles (1-15)
-;   L' = Minimum CS2 External I/O Bus Cycles (1-15)
 ;
 ; Output
-;   H = CS3 External Memory Bus Cycles (1-15)
-;   L = CS2 External I/O Bus Cycles (1-15)
-;   A = 0 -> SUCCESS, NZ -> ONE OR MORE VALUES OUT OF RANGE
 ;
-ez80_bus_freq_set:
-	; TO CALCULATE BUS CYCLES FOR A DESIRED BUS FREQUENCY WE USE:
-	; CYCLES = (_cpu_freq_calculated / 1000) / target_bus_frequency_khz;
-	; FACTOR THE VALUES TO ENABLE ROUNDING TO NEAREST WHOLE CYCLE:
-	; (((_cpu_freq_calculated/100) / target_bus_frequency)+5)/10;
 
-	PUSH	IX
-	LD	IX, 0
-	ADD	IX, SP
+;
+; Function B = 04 --DEPRECATED
+;
+; Input
+;
+; Output
+;
 
-	PUSH	HL					; PERSIST DESIRED CS3 BUS FREQ (IX-3)
-	PUSH	DE					; PERSIST DESIRED CS2 BUS FREQ (IX-6)
-	PUSH	BC					; RESERVE 3 BYTES FOR TEMP STORAGE (IX-9)
-	DEC	SP					; RESERVE 2 BYTE FOR TEMP STORAGE (IX-10)
 
-	LD	HL, (_cpu_freq_calculated)
-	LD	A, (_cpu_freq_calculated + 3)
-	LD	E, A
-	LD	BC, 100
-	XOR	A
-	CALL	__ldivu
-	LD	(IX-9), HL				; HL = _cpu_freq_calculated / 100
-
-	LD	BC, (IX-3)				; BC = DESIRED CS3 BUS FREQ
-	CALL	__stoiu
-	LD	BC, HL					; CONVERTED TO 24 BITS
-
-	XOR	A
-	LD	HL, (IX-9)				; HL = _cpu_freq_calculated / 100
-	CALL	__ldivu					; HL = HL / BC
-
-	LD	A, 5					; HL = HL + 5
-	CALL	__ladd_b
-
-	LD	BC, 10					; HL = HL / 10
-	XOR	A
-	CALL	__ldivu
-	LD	(IX-10), L				; L = CALCULATED CS3 BUS CYCLES
-
-	LD	BC, (IX-6)				; DESIRED CS2 BUS FREQ
-	CALL	__stoiu
-	LD	BC, HL					; CONVERTED TO 24 BITS
-
-	XOR	A
-	LD	HL, (IX-9)				; _cpu_freq_calculated / 100
-	CALL	__ldivu
-
-	LD	A, 5					; HL = HL + 5
-	CALL	__ladd_b
-
-	LD	BC, 10					; HL = HL / BC
-	XOR	A
-	CALL	__ldivu					; L = CALCULATED CS2 BUS CYCLES
-
-	LD	H, (IX-10)				; CALCULATED CS3 BUS CYCLES
-
-	EXX
-	LD	A, L					; IO must always be >= L'
-	EXX
-	CP	L
-	JR	C, valid_io_fq
-	LD	L, A
-valid_io_fq:
-	EXX
-	LD	A, H
-	EXX
-	CP	H					; Mem must always be >= H'
-	JR	C, valid_mem_fq
-	LD	H, A
-valid_mem_fq:
-
-	LD	SP, IX
-	POP	IX
-	JR	ez80_bus_cycles_set
 ;
 ; Function B = 05 -- SYSUTL_CPU_FREQ_GET
 ; Get CPU Frequency
@@ -332,33 +206,13 @@ ez80_cpu_freq_get:
 
 	RET.L
 ;
-; Function B = 06 -- SYSUTL_BANK_HELPER_SET
-;
-; Set the type of memory banking used for the external memory access
-; This function will control the banking operations for RST %18 helper
-;
-; Only support memory banking model supported is RomWBW(1)/MM_Z2(2)
+; Function B = 06 -- DEPRECATED
 ;
 ; Inputs:
-;  C 	= Banking type -(Code as per platform type)
-;  HL	= Rom KB size
-;  DE	= Ram KB size
 ;
 ; Outputs:
-; A 	= 0 -> SUCCESS, NZ -> helper support not available for requested platform
 ;
-ez80_bank_helper_set:
-	LD	A, C
-	CP	ROMWBW_MM_Z2
-	JR	NZ, ez80_bank_helper_set_not_supported
 
-	XOR	A				; SUCCESS
-	RET.L
-
-ez80_bank_helper_set_not_supported:
-	LD	A, 1
-	OR	A
-	RET.L
 ;
 ; Function B = 07 -- SYSUTL_DEBUG
 ; Convenience function to allow breakpoint to be set within ZDS
