8800693fe458e0eec4e0a0e29c1e245367dd2849
Dean Netherton <dinoboards@netherton.org>
2024-10-05 13:12:55 +1000

    firmware: fix issue where z80 test function incorrectly overwrote code in ram

--- firmware/src/test.s
+++ firmware/src/test.s
@@ -7,26 +7,28 @@
 
 	public	_spike
 
+	XREF	__INTERNAL_RAM_RAM
+	XREF	__INTERNAL_RAM_LEN
 _spike:
 	ei
 
 	; write spike code here to be run in ADL mode on  on-chip rom
 
+RAM_FREE	.EQU	__INTERNAL_RAM_RAM+__INTERNAL_RAM_LEN
+RAM_FREE_BASE	.EQU	RAM_FREE & %FFFF
 
 	; copy code from rom to ram and run it in Z80 mode
 	LD	HL, step1
-	ld	de, 02E300h
+	ld	de, RAM_FREE
 	ld	bc, step1_size
 	ldir
 
 	LD	A, 02H
 	LD	MB, A
-	LD.SIS	SP, 0E700H
+	LD.SIS	SP, 0EA00H
 	NOP
 	NOP
-	LD	HL, %451234
-	LD	E, %66
-	CALL.IS	0E300H		; run the z80 code in RAM
+	CALL.IS	RAM_FREE_BASE		; run the z80 code in RAM
 	RET
 
 
@@ -35,6 +37,8 @@ _spike:
 	.assume adl = 0
 step1:
 
+	NOP
+	JR	step1
 	; write code here to be run in z80 mode on on-chip rom
 
 step1_end:
