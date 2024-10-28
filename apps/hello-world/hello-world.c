8c497d708309abf415739ba1f6021447798ac430
Dean Netherton <dinoboards@netherton.org>
2024-10-28 19:35:53 +1100

    apps: added hello-world to set of app - compiled with clang

---
+++ apps/hello-world/hello-world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main() {
+  uint24_t a = 1;
+  uint24_t b = 2;
+  uint24_t c = a + b;
+
+  printf("Hello, World! 1+2 is %d\r\n", c);
+}
+
---
+++ apps/hello-world/readme.md
@@ -0,0 +1,6 @@
+A clang compiled hello world
+
+Using the ez80 clang tool chain
+
+compiled to ez80 ADL code
+
