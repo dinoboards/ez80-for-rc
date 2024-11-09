d9e747747938f5389f5c6d0c3493fb875ab6c705
Dean Netherton <dinoboards@netherton.org>
2024-11-09 11:27:43 +1100

    apps: confirming strcpy and strchr exist in clib

--- apps/hello-world-adl/hello-world.c
+++ apps/hello-world-adl/hello-world.c
@@ -1,12 +1,28 @@
 #include <stdint.h>
 #include <stdio.h>
-
+#include <string.h>
 char input[4];
+
+void test_strcpy() {
+  char src[] = "Hello, World!";
+  char dst[14];
+  strcpy(dst, src);
+  printf("dst: %s\r\n", dst);
+}
+
+void test_strchr() {
+  char str[] = "Hello, World!";
+  char *ptr = strchr(str, 'W');
+  printf("ptr: %s\r\n", ptr);
+}
 
 int main(void) {
   uint24_t a = 1;
   uint24_t b = 2;
   uint24_t c = a + b;
+
+  test_strcpy();
+  test_strchr();
 
   printf("Hello, World! 1+2 is %d\r\n", c);
 
