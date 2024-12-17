30c306ee1875f4b58dd873a85b4be798183c99e5
Dean Netherton <dinoboards@netherton.org>
2024-12-17 13:54:07 +1100

    apps:ez80: added support for managing tick timer

--- apps/ez80/build.sh
+++ apps/ez80/build.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-echo "Compiling find-extended-memory.c -> find-extended-memory.s"
-ez80-clang ${INCLUDE_PATHS} -nostdinc -ffunction-sections -fdata-sections -O3 -Wall -Wextra -Wunreachable-code -Werror -mllvm -z80-print-zero-offset -S find-extended-memory.c -c -o find-extended-memory.s
+echo "Compiling report-memory-timing.c -> report-memory-timing.s"
+ez80-clang ${INCLUDE_PATHS} -nostdinc -ffunction-sections -fdata-sections -O3 -Wall -Wextra -Wunreachable-code -Werror -mllvm -z80-print-zero-offset -S report-memory-timing.c -c -o report-memory-timing.s
 
--- apps/ez80/ez80.h
+++ apps/ez80/ez80.h
@@ -15,9 +15,11 @@ extern void report_memory_timing(void);
 extern void show_help(void);
 extern void validate_mem_set_value(const char *value, mem_config_t *mem_config);
 extern void validate_wait_only_set_value(const char *value, mem_config_t *mem_config);
+extern void validate_number(const char *value, uint8_t *number);
 extern void config_flash(mem_config_t mem_config);
 extern void config_mem0(mem_config_t mem_config);
 extern void config_mem(mem_config_t mem_config);
 extern void config_io(mem_config_t mem_config);
+extern void config_set_tick_rate(uint8_t rate);
 
 #endif
