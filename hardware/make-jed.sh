ce9f793bc4e3771d2cef30d254ce2d2e2bcb6947
Dean Netherton <dinoboards@netherton.org>
2025-01-29 08:11:57 +1100

    spiking wincupl for atf150x

--- hardware/make-jed.sh
+++ hardware/make-jed.sh
@@ -11,7 +11,7 @@ WINCUPLPATH=/Wincupl/Shared/
 
 mkdir -p ./bin
 
-wine ${WINCUPLPATH}cupl.exe -e -x -f - -ju ${WINCUPLPATH}\cupl.dl $NAME 2>>./bin/wincuplerror.log
+wine ${WINCUPLPATH}cupl.exe -a -l -m4 -e -x -f - -ju ${WINCUPLPATH}\cupl.dl $NAME 2>>./bin/wincuplerror.log
 
 # Removed the location entry - the TL866II Plus programmer seems to have loading file issue
 sed -i '/Location/d' ./${NAME}.jed
@@ -19,3 +19,13 @@ sed -i '/Location/d' ./${NAME}.jed
 mv ${NAME}.jed ./bin
 mv ${NAME}.doc bin/${NAME}.txt
 mv ${NAME}.mx bin/${NAME}.mx
+
+[ -f "${NAME}.fit" ] && mv ${NAME}.fit bin/${NAME}.fit
+[ -f "${NAME}.io" ] && mv ${NAME}.io bin/${NAME}.io
+[ -f "${NAME}.abs" ] && mv ${NAME}.abs bin/${NAME}.abs
+[ -f "${NAME}.pin" ] && mv ${NAME}.pin bin/${NAME}.pin
+[ -f "${NAME}.pla" ] && mv ${NAME}.pla bin/${NAME}.pla
+[ -f "${NAME}.sim" ] && mv ${NAME}.sim bin/${NAME}.sim
+[ -f "${NAME}.tt2" ] && mv ${NAME}.tt2 bin/${NAME}.tt2
+[ -f "${NAME}.tt3" ] && mv ${NAME}.tt3 bin/${NAME}.tt3
+
