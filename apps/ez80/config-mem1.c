b832fd3b94e8ac044b7b475d582696b9196a83f6
Dean Netherton <dinoboards@netherton.org>
2025-03-29 20:42:10 +1100

    apps: update ez80-clang required version to 0.0.13-rc.94

--- apps/ez80/config-mem1.c
+++ apps/ez80/config-mem1.c
@@ -4,19 +4,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-extern uint8_t ez80_mem1_bus_cycles_setxx(const uint8_t cycles);
-
 void config_mem1(mem_config_t mem_config) {
   if (mem_config.type == WAIT_STATE) {
     printf("Setting Extended Memory CS1 Wait States to %d\r\n", mem_config.value);
-    ez80_mem1_bus_cycles_setxx(mem_config.value);
+    ez80_mem1_bus_cycles_set(mem_config.value);
 
     return;
   }
 
   if (mem_config.type == BUS_CYCLE) {
     printf("Setting Extended Memory CS1 Bus Cycles to %d\r\n", mem_config.value);
-    ez80_mem1_bus_cycles_setxx(mem_config.value | 0x80);
+    ez80_mem1_bus_cycles_set(mem_config.value | 0x80);
 
     return;
   }
