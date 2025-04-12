05a3582f4ce9d7dffe3f83d38e3ba68355a1ee75
Dean Netherton <dinoboards@netherton.org>
2025-04-12 10:56:52 +1000

    apps: updated exe to support marshalling hbios calls - hello-world updated to verify hbios serial write

--- apps/exe/marshaller.asm
+++ apps/exe/marshaller.asm
@@ -16,16 +16,22 @@ _start_marshalling:
 
 	.space 5-(.-_start_marshalling)	;skip 1 byte
 
-	; ORG 0x200005
+	; ORG $xx0005
 _start_cpm_bdos_adl:
 	CALL.sis cpm_bdos_z80 & 0xFFFF
 	RET
 
-	;ORG 0x20000A
+	;ORG $xx000A
 _extended_memory_start:
 	D24	0	; replaced at load time
 
+	;ORG $xx000D
 _extended_memory_end:
 	D24	0	; replaced at load time
+
+	;ORG $xx0010
+; hbios_adl:
+	CALL.sis hbios_z80 & 0xFFFF
+	RET
 
 _end_marshalling:
