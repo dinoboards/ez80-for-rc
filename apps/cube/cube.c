e1e02f09b0b41e313b240e3063ea503c13176a63
Dean Netherton <dinoboards@netherton.org>
2025-07-15 21:01:42 +1000

    apps: cube: added menu option to select graphics mode and removed exe version

--- apps/cube/cube.c
+++ apps/cube/cube.c
@@ -12,11 +12,10 @@
 #define WIDTH  (RIGHT - LEFT)
 #define HEIGHT (BOTTOM - TOP)
 
-#ifdef VDP_SUPER_HDMI
-#define PAGE_HEIGHT ((int24_t)vdp_get_screen_height())
-#else
-#define PAGE_HEIGHT 256
-#endif
+uint16_t page_height;
+uint8_t change_colour = true;
+uint8_t graphic_mode;
+uint8_t super_mode = false;
 
 #define cx   (WIDTH / 2)
 #define cy   (HEIGHT / 2)
@@ -47,7 +46,7 @@ void erase_page_0() {
 
 void erase_page_1() {
   vdp_cmd_wait_completion();
-  vdp_cmd_logical_move_vdp_to_vram(0, PAGE_HEIGHT, vdp_get_screen_width(), vdp_get_screen_height(), 0, 0, 0);
+  vdp_cmd_logical_move_vdp_to_vram(0, page_height, vdp_get_screen_width(), vdp_get_screen_height(), 0, 0, 0);
 }
 
 void erase_current_page() {
@@ -64,7 +63,7 @@ void page_draw_line(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint8_t 
     vdp_draw_line(x1, y1, (x2), y2, color, CMD_LOGIC_IMP);
   } else {
     vdp_cmd_wait_completion();
-    vdp_draw_line(x1, y1 + PAGE_HEIGHT, x2, y2 + PAGE_HEIGHT, color, CMD_LOGIC_IMP);
+    vdp_draw_line(x1, y1 + page_height, x2, y2 + page_height, color, CMD_LOGIC_IMP);
   }
 }
 
@@ -102,7 +101,7 @@ uint8_t edges[EDGES][2] = {
 
 int24_t timeDelta, timeLast = 0;
 
-uint8_t colour = 0;
+uint8_t colour = 1;
 
 void draw_cube(int24_t timeNow) {
 
@@ -153,7 +152,8 @@ void draw_cube(int24_t timeNow) {
     uint8_t *edge = edges[i];
     page_draw_line(vertices[edge[0]].x, vertices[edge[0]].y, vertices[edge[1]].x, vertices[edge[1]].y, colour);
   }
-  colour++;
+  if (change_colour)
+    colour++;
 
   swap_page();
 }
@@ -169,14 +169,110 @@ void init() {
   vertices[7] = (POINT3D){cx - size, cy + size, cz + size};
 }
 
+
+char input[4];
+
+void set_video_mode(void) {
+
+retry:
+  printf("Choose Graphic Mode\r\n");
+  printf("a: V9958 Graphics Mode 6\n");
+  printf("b: V9958 Graphics Mode 7\n");
+
 #ifdef VDP_SUPER_HDMI
+  printf("c: Super Graphics Mode 1\n");
+  printf("d: Super Graphics Mode 2\n");
+  printf("e: Super Graphics Mode 3\n");
+  printf("f: Super Graphics Mode 4\n");
+  printf("g: Super Graphics Mode 5\n");
+  printf("h: Super Graphics Mode 6\n");
+  printf("i: Super Graphics Mode 7\n");
+  printf("j: Super Graphics Mode 8\n");
+  printf("k: Super Graphics Mode 9\n");
+#endif
+
+  printf("\n?: ");
+  fgets(input, 4, stdin);
+  printf("\r\n");
+
+  char sel = input[0];
+  if (sel >= 'A' && sel <= 'Z') sel += 'a' - 'A'; // to lower
+
+  switch (sel) {
+    case 'a':
+      vdp_set_graphic_6();
+      break;
+    case 'b':
+      vdp_set_graphic_7();
+      break;
+
+#ifdef VDP_SUPER_HDMI
+    case 'c':
+      vdp_set_super_graphic_1();
+      break;
+    case 'd':
+      vdp_set_super_graphic_2();
+      break;
+    case 'e':
+      vdp_set_super_graphic_3();
+      break;
+    case 'f':
+      vdp_set_super_graphic_4();
+      break;
+    case 'g':
+      vdp_set_super_graphic_5();
+      break;
+    case 'h':
+      vdp_set_super_graphic_6();
+      break;
+    case 'i':
+      vdp_set_super_graphic_7();
+      break;
+    case 'j':
+      vdp_set_super_graphic_8();
+      break;
+    case 'k':
+      vdp_set_super_graphic_9();
+      break;
+#endif
+
+    default:
+      printf("Invalid selection. Try again.\n");
+      goto retry;
+  }
+
+#ifdef VDP_SUPER_HDMI
+  if (sel >= 'c') {
+    graphic_mode = sel - 'c' + 1;
+    vdp_set_extended_palette(large_palette);
+    page_height = ((int24_t)vdp_get_screen_height());
+    change_colour = true;
+    super_mode = true;
+  }
+  else {
+#endif
+    graphic_mode = sel - 'a' + 1;
+    vdp_set_lines(212);
+    vdp_set_refresh(PAL);
+    page_height = 256;
+    change_colour = false;
+    colour = 2;
+    super_mode = false;
+#ifdef VDP_SUPER_HDMI
+  }
+#endif
+}
+
 int main(/*const int argc, const char *argv[]*/) {
 
   vdp_init();
-  vdp_set_super_graphic_1();
-  vdp_set_extended_palette(large_palette);
+  set_video_mode();
 
-  printf("Super Graphics Mode 1 (%d x %d), 256 Colours\r\n", vdp_get_screen_width(), vdp_get_screen_height());
+  printf("%sGraphics Mode %d (%d x %d)%s\r\n",
+    (super_mode ? "Super " : ""),
+    graphic_mode,
+    vdp_get_screen_width(), vdp_get_screen_height(),
+    (super_mode ? ", 256 Colours" : ""));
 
   init();
 
@@ -196,29 +292,3 @@ int main(/*const int argc, const char *argv[]*/) {
     time += 10;
   }
 }
-
-#else
-int main(/*const int argc, const char *argv[]*/) {
-
-  vdp_init();
-  vdp_set_lines(212);
-  vdp_set_refresh(PAL);
-  vdp_set_graphic_7();
-
-  init();
-
-  erase_page_0();
-  vdp_cmd_wait_completion();
-  erase_page_1();
-
-  vdp_set_page(0);
-
-  uint24_t time = 0;
-  while (true) {
-    if (cpm_c_rawio() != 0)
-      return 0;
-    draw_cube(time);
-    time += 10;
-  }
-}
-#endif
