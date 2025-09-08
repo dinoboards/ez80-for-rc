0f4eddeeac19d0cf632a4956f2657ad1267ed561
Dean Netherton <dinoboards@netherton.org>
2025-09-08 15:47:49 +1000

    firmware/vdu: refactor vdu_16 (clg)

--- firmware/src/q3vm/src/vdu/vdu_16.c
+++ firmware/src/q3vm/src/vdu/vdu_16.c
@@ -6,12 +6,11 @@ void vdu_clg(void) {
   // for moment lets just erase to black
   // TODO: apply correct back colour
 
-  const int24_t  left   = convert_x(gviewport.left);
-  const int24_t  top    = convert_y(gviewport.top);
-  const uint24_t width  = convert_x(gviewport.right) - left + 1;
-  const uint24_t height = convert_y(gviewport.bottom) - top + 1;
+  const int24_t left = convert_x(gviewport.left);
+  const int24_t top  = convert_y(gviewport.top);
 
-  vdp_cmd_logical_move_vdp_to_vram(left, top, width, height, 0, 0, 0);
+  vdp_cmd_logical_move_vdp_to_vram(left, top, convert_x(gviewport.right) - left + 1, convert_y(gviewport.bottom) - top + 1, 0, 0,
+                                   0);
 
   current_gpos.x = 0;
   current_gpos.y = 0;
