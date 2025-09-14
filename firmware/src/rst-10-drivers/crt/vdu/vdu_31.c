8733bf3cfb08c535d475c37e98f9047a4a63bc99
Dean Netherton <dinoboards@netherton.org>
2025-09-15 09:49:44 +1000

    firmware/vdu: fixed vdu_31 (tab) to correctly position text cursor

--- firmware/src/rst-10-drivers/crt/vdu/vdu_31.c
+++ firmware/src/rst-10-drivers/crt/vdu/vdu_31.c
@@ -1,6 +1,6 @@
 #include "../vdu.h"
 
 void vdu_tab(void) {
-  current_tpos.x = data[0];
-  current_tpos.y = data[1];
+  current_tpos.x = (int24_t)data[0] * 8;
+  current_tpos.y = (int24_t)data[1] * 8;
 }
